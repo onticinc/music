a0c4ae8fe7abf88bf6bd38c167f7b26dcc5962d7
onticinc <onticinc@gmail.com>
2023-10-22 16:49:52 -0600

    setting up sidebar functionality

---
+++ components/Library.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import exp from "constants"
+
+const Library = () => {
+
+    const onClick = () => {
+        // handle upload later
+    }
+    return (
+        <div>
+            Library
+        </div>
+    )
+}
+
+export default Library
