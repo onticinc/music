3a38acd03a719f38bc7e369972e0034212a85322
onticinc <onticinc@gmail.com>
2023-10-22 23:55:18 -0600

    layout for sidebar

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,17 +1,28 @@
-"use client"
+"use client";
 
-import exp from "constants"
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Library = () => {
+  const onClick = () => {
+    // handle upload later
+  };
+  return (
+    <div className="flex flex-col">
+        <div className="flex items-center justify-between px-5 pt-4">
 
-    const onClick = () => {
-        // handle upload later
-    }
-    return (
-        <div>
-            Library
+            <div className="inline-flex items-center gap-x-2">
+                <TbPlaylist className="text-neutral-400" size={26} />
+                <p className="text-neutral-400 font-medium text-md">Your Library</p>
+            </div>
+            <AiOutlinePlus className="text-neutral-400 hover:text-white transition cursor-pointer" size={20} onClick={onClick}/>
         </div>
-    )
-}
+        <div className="flex clex-col gap-y-2 px-3 mt-4">
 
-export default Library
+
+        </div>
+    </div>
+  );
+};
+
+export default Library;
