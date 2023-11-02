b291de3fc376dfc0dbd0d633f5c8b3b1784cb352
onticinc <onticinc@gmail.com>
2023-11-01 22:16:36 -0600

    setup UserProvider.ts

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,5 +1,5 @@
 import { User } from "@supabase/auth-helpers-nextjs";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 import { Subscription, UserDetails } from "@/types";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
@@ -75,4 +75,12 @@ export const MyUserContextProvider = (props: Props) => {
     }
 
     return <UserContext.Provider value={value} {...props} />
-} 
+}; 
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a UserContextProvider');
+    }
+    return context;
+};
